ed8633d614fa03ce89f124dd160892ad920163ec
Ankit Jagtap <ankit.jagtap50@gmail.com>
2024-06-27 17:22:59 +0530

    Add project files.

---
+++ SecondAPIProject/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SecondAPIProject.Models
+{
+    public class Employee
+    {
+        public int Empid { get; set; }
+        [Required(ErrorMessage ="Name is compulsory",AllowEmptyStrings =false)]
+        public string Name { get; set; }
+
+
+    }
+}
